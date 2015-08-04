d8ebfabee89c7b5d35ac1f64b1d5b945cccde06f
Renan Couto <renancouto@gmail.com>
2015-08-04 08:24:21 -0300

    updated - `main-header`

--- gulp/imagemin.js
+++ gulp/imagemin.js
@@ -6,7 +6,6 @@
 var gulp = require('gulp')
 var plumber = require('gulp-plumber')
 var notify = require('gulp-notify')
-var newer = require('gulp-newer')
 var imagemin = require('gulp-imagemin')
 
 /**
@@ -15,7 +14,6 @@ var imagemin = require('gulp-imagemin')
 gulp.task('imagemin', function(){
   return gulp.src('./src/img/*.{jpg,png,gif,svg}')
     .pipe(plumber({ errorHandler: notify.onError('MINIFY IMAGES Error: <%= error.message %>') }))
-    .pipe(newer('./src/img'))
     .pipe(imagemin())
     .pipe(gulp.dest('./src/img'))
 })
--- gulp/svg.js
+++ gulp/svg.js
@@ -7,6 +7,8 @@ var gulp = require('gulp')
 var svgSprite = require('gulp-svg-sprite')
 var plumber = require('gulp-plumber')
 var notify = require('gulp-notify')
+var replace = require('gulp-replace')
+var del = require('del')
 
 /**
  * settings
@@ -14,15 +16,16 @@ var notify = require('gulp-notify')
 var settings = {
   shape: {
     dimension: {
-      maxWidth: 32,
-      maxHeight: 32
+      maxWidth: 16,
+      maxHeight: 16
     },
     spacing: {
-      padding: 10
+      padding: 4
     }
   },
   mode: {
     view: {
+      prefix: '.u-svg-%s',
       bust: false,
       render: {
         styl: true
@@ -32,11 +35,26 @@ var settings = {
 }
 
 /**
- * svg task
+ * tasks
  */
-gulp.task('svg', function () {
+gulp.task('svg:sprite', function () {
   return gulp.src('./src/svg/*.svg')
     .pipe(plumber({ errorHandler: notify.onError('SVG Error: <%= error.message %>') }))
     .pipe(svgSprite(settings))
+    .pipe(gulp.dest('./src/svg'))
+})
+
+gulp.task('svg:copycss', [ 'svg:sprite' ], function () {
+  return gulp.src('./src/svg/view/*.styl')
+    .pipe(replace('svg/sprite', '/img/sprite'))
+    .pipe(gulp.dest('./src/css/util'))
+})
+
+gulp.task('svg:copysvg', [ 'svg:sprite' ], function () {
+  return gulp.src('./src/svg/view/svg/*.svg')
     .pipe(gulp.dest('./src/img'))
+})
+
+gulp.task('svg', [ 'svg:copycss', 'svg:copysvg' ], function (next) {
+  del('./src/svg/view', next)
 })

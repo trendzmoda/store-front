e7aeee058b327825279ecbfd9aeca3af6896536e
Renan Couto <renancouto@gmail.com>
2015-08-10 23:10:30 -0300

    added - `product detail` draft

--- data/showcase.js
+++ data/showcase.js
@@ -9,14 +9,18 @@ var _ = require('lodash')
  * settings
  */
 var items = [{
+  id: 1,
   name: 'Foo',
   description: 'Lorem ipsum dolor sit amet',
   url: '/product.html',
   image: 'http://lorempixel.com/225/300/fashion/1',
+  imageLarge: 'http://lorempixel.com/525/800/fashion/1',
+  galleryCount: 5,
   price: 79.90,
   listPrice: 89.90,
   times: 2
 }, {
+  id: 2,
   name: 'Bar',
   description: 'Consectetur adipisicing elit',
   url: '/product.html',
@@ -25,6 +29,7 @@ var items = [{
   listPrice: 549.90,
   times: 5
 }, {
+  id: 3,
   name: 'Baz',
   description: 'Dolor consectetur ipsum',
   url: '/product.html',
